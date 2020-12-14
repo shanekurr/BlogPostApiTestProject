7b5d7376a057837d5ee3097869242a7f5cca8be0
Shane <shane.kurr@gmail.com>
2020-12-13 19:23:13 -0800

    Created base project, with UserPost class. Added unit test skeleton.

---
+++ FieldLevel/UserPost.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace FieldLevel
+{
+    public class UserPost
+    {
+        public DateTime RetrievedAt { get; set; }
+
+        public string Post { get; set; }
+
+        public int UserId { get; set; }
+
+    }
+}
---
+++ FieldLevelTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FieldLevel.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FieldLevel.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
