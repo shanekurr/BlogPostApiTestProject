9a514bb2264648f8a46c444c1a5100dad2ba8cca
Shane <shane.kurr@gmail.com>
2020-12-16 15:02:28 -0800

    Calling datasource, working unit test

--- FieldLevel/Controllers/HomeController.cs
+++ FieldLevel/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -13,16 +14,20 @@ namespace FieldLevel.Controllers
     {
         
         private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _client;
+        private const string DATA_SOURCE_URL = "https://jsonplaceholder.typicode.com/posts";
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, HttpClient client)
         {
             _logger = logger;
+            _client = client;
         }
 
         [HttpGet]
-        public IEnumerable<UserPost> Get()
+        public async Task<HttpResponseMessage> Get()
         {
-            return null;
+            var result = await _client.GetAsync(DATA_SOURCE_URL);
+            return result;
         }
     }
 }
--- FieldLevelTests/Controllers/HomeControllerTests.cs
+++ FieldLevelTests/Controllers/HomeControllerTests.cs
@@ -3,6 +3,9 @@ using FieldLevel.Controllers;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Threading.Tasks;
 
 namespace FieldLevel.Controllers.Tests
 {
@@ -10,9 +13,13 @@ namespace FieldLevel.Controllers.Tests
     public class HomeControllerTests
     {
         [TestMethod()]
-        public void GetTest()
+        public async Task GetTest()
         {
-            Assert.Fail();
+
+            var testController = new HomeController(new NullLogger<HomeController>(), new System.Net.Http.HttpClient());
+            var response = await testController.Get();
+            var result = await response.Content.ReadAsStringAsync();
+            Assert.IsNotNull(result);
         }
     }
 }
